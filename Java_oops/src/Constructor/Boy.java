81a30da7d25a08cf1097999f9f6784f1ec465c82
Suresh Nemani <chandra.nemani@gmail.com>
2016-07-12 19:28:04 +0530

    constructor in inheritance context

---
+++ Java_oops/src/Constructor/Boy.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Constructor;
+
+
+public class Boy extends Human{
+
+	/**
+	 * boy class extends human class
+	 * when we creating object of child class then first super class /parent class constructor will be invoked first then 
+	 * child class constructor
+	 * 
+	 */
+	
+	Boy(){
+		//here we are overriding s2 value in the child class constructor
+		s2="child class";
+	}
+	
+	public void print(){
+		System.out.println(s1+" "+s2);
+	}
+	public static void main(String[] args) {
+		Boy b= new Boy();//creating object to child class
+		b.print();
+		
+
+	}
+
+}
---
+++ Java_oops/src/Constructor/Human.java
@@ -0,0 +1,15 @@
+package Constructor;
+
+public class Human {
+	
+	
+	String s1,s2;//declaring variables of string type
+	Human(){
+		//intlialising variables on the constructor
+		s1="super class";
+		s2="parent class";
+		
+		
+	}
+
+}
