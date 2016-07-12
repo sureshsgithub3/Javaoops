7d7228f634a5e6f1ac5b128080c2b7c65bc72d93
Suresh Nemani <chandra.nemani@gmail.com>
2016-07-12 13:40:52 +0530

    polymorphism concept

---
+++ Java_oops/src/Polymorphism/polymorphismConcepts.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package Polymorphism;
+
+/**Polymorphism in java is a concept by which we can perform a single action by different ways.
+ * There are two types of polymorphism in java: compile time polymorphism and runtime polymorphism.
+ * We can perform polymorphism in java by method overloading and method overriding.
+ * If you overload static method in java, it is the example of compile time polymorphism.
+ * Runtime polymorphism:is a process in which a call to an overridden method is resolved at runtime rather than compile-time.
+    In this process, an overridden method is called through the reference variable of a superclass
+ */
+public class polymorphismConcepts {
+
+}
